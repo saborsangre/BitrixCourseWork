e61549a8ac4771b664c1bc1beb7bf6840d6536e3
saborsangre <saborsangre@gmail.com>
2016-12-16 18:57:47 +0300

    Инициализация репозитория интернет-магазина

---
+++ .section.php
@@ -0,0 +1,8 @@
+<?
+$sSectionName = "Главная страница";
+$arDirProperties = array(
+   "description" => "Мы предлагаем широкий ассортимент электроники адекватным ценам.",
+   "keywords" => "электроника, телефоны, планшеты",
+   "robots" => "index, follow"
+);
+?>
